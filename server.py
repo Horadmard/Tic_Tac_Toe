6aa033d3dd8ed3d6bdfae2b78785bf20c2d4ef4d
Horadmard <iranradmard1@gmail.com>
2024-01-05 02:39:55 +0330

    add turn

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket 
 import threading
- 
+
+player_X_turns = True
+
 # Function to handle each client connection 
 def handle_client(client, player, client2): 
     while True: 
@@ -12,9 +14,12 @@ def handle_client(client, player, client2):
                 break 
 
             print(f"Received from Player {player}: {list(data)}")
-            
-            client.sendall(data)
-            client2.sendall(data)
+            if (player == 'x' and player_X_turns) or (player == 'O' and not player_X_turns):
+                client.sendall(data)
+                client2.sendall(data)
+                player_X_turns = not player_X_turns
+
+
                 
             
         except Exception as e: 
@@ -36,15 +41,17 @@ def setup_server():
     print(f"Server listening on {host}:{port}") 
  
     # Accept two client connections 
-    player1, addr1 = server.accept() 
-    print(f"Player 1 connected from {addr1}") 
+    # player1, addr1 = server.accept() 
+    client_X = server.accept()
+    print(f"Player 1 connected from {client_X[1]}") 
  
-    player2, addr2 = server.accept() 
-    print(f"Player 2 connected from {addr2}") 
+    # player2, addr2 = server.accept()
+    client_O = server.accept() 
+    print(f"Player 2 connected from {client_O[1]}")
  
     # Start a thread for each client 
-    threading.Thread(target=handle_client, args=(player1, 1, player2)).start()
-    threading.Thread(target=handle_client, args=(player2, 2, player1)).start() 
+    threading.Thread(target=handle_client, args=(client_X[0], 'X', client_O[0])).start()
+    threading.Thread(target=handle_client, args=(client_O[0], 'O', client_X[0])).start() 
  
 if __name__ == "__main__":
     setup_server()
